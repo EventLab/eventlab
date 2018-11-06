31da937b98e99fbbb29d4ebea391635e983800e7
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2018-11-06 22:04:46 +0100

    Create EventLab source namespace

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Kernel;
+use EventLab\UserInterface\Symfony\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
